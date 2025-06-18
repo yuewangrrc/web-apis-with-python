cd4f410f4e33cd24d716549f04c9961a360728be
yuewangrrc <yuewang@academic.rrc.ca>
2025-06-18 13:20:02 -0500

    greetapi

--- app.py
+++ app.py
@@ -14,4 +14,23 @@ def index():
     4. If first name is provided byt second name is not provided: respond with "Hello, <first-name>!"
     5. If both names are provided: respond with a question, "Is your name <fist-name> <second-name>
     """
-    return jsonify("TODO")
+    
+    # name = request.args.get("name")
+    lname = request.args.get("lname")
+    fname = request.args.get("fname")
+    # if not name:
+    #     return jsonify({"error": "Name parameter is required"}), 400
+    # response = {"data": f"Hello, {name}!"}
+    # return jsonify(response)
+    # Check if both first name and last name are provided
+    if not fname and not lname:
+        return jsonify({"error": "First name and last name are required"}), 400
+    elif not fname and lname:
+    # Check if only last name is provided
+        return jsonify({"data": f"Hello Mr {lname}!"})
+    # Check if only first name is provided
+    elif fname and not lname:
+        return jsonify({"data": f"Hello, {fname}!"})
+    # If both names are provided
+    else:
+        return jsonify({"data": f"Is your name {fname} {lname}?"})
